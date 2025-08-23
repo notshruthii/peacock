c26f6c868301c935b8b64716cdedccc18458b5c6
shruthi <shruthigupta1404@gmail.com>
2025-08-23 13:34:49 +0530

    navbar pages done

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const Product = require("./models/product");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("frontend")); // for HTML/CSS/JS
+app.use(express.static("public")); 
 // Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/ecommerce", {
   useNewUrlParser: true,
