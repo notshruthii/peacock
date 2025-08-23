32388cab1a2baee1997a03b4f9ab3fe66513f2f8
shruthi <shruthigupta1404@gmail.com>
2025-08-23 16:46:32 +0530

    completed all frontend

---
+++ frontend/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUbtFS7eKpj5Rq7P-Ygf4ScQtJINqjHC0",
+  authDomain: "kalaghar-95af5.firebaseapp.com",
+  projectId: "kalaghar-95af5",
+  storageBucket: "kalaghar-95af5.firebasestorage.app",
+  messagingSenderId: "886508190110",
+  appId: "1:886508190110:web:8c79a5eca8177d7ce93e8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
